411cb4cecc2174a10d99ef0b2174d97900eb2971
RAHUL <rahulsd380@gmail.com>
2025-05-19 16:09:08 +0600

    Added sidebar links and header

--- src/layouts/DashboardLayout/DashboardLayout.tsx
+++ src/layouts/DashboardLayout/DashboardLayout.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../../components/Dashboard/Sidebar/Sidebar";
+import DashboardHeader from "../../components/Dashboard/DashboardHeader/DashboardHeader";
 
 const DashboardLayout = () => {
   return (
     <div className="bg-primary-40 flex">
       <Sidebar />   
-      <div className="w-full px-5 py-[30px]">
+      <div className="w-full">
+        <DashboardHeader/>
+        <div className="px-5 py-[30px]">
         <Outlet />
+        </div>
       </div>
     </div>
   );
--- src/pages/Dashboard/DashboardHome/DashboardHome.tsx
+++ src/pages/Dashboard/DashboardHome/DashboardHome.tsx
@@ -3,13 +3,13 @@ import Heading from "../../../components/Reusable/Heading";
 const DashboardHome = () => {
   return (
     <div className="font-Outfit">
-      <Heading 
-  iconSrc="/assets/dashboard.svg"
-  title="Dashboard"
-  subtitle="Overview of your activities"
-  Showbtn={true}
-  // rightContent={<button className="text-white bg-blue-500 px-4 py-2 rounded">Action</button>}
-/>
+      <Heading
+        iconSrc="/assets/dashboard.svg"
+        title="Dashboard"
+        subtitle="Overview of your activities"
+        Showbtn={true}
+        // rightContent={<button className="text-white bg-blue-500 px-4 py-2 rounded">Action</button>}
+      />
     </div>
   );
 };
